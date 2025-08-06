0bcc6f1bd874a3f6eb813d966466237b0e883235
diaasaeed <O-DIAA.SAIED@etisalat.com>
2025-08-06 21:29:46 +0300

    Initial Commit

---
+++ TRU Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TRU Assignment
+//
+//  Created by Diaa saeed on 06/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
